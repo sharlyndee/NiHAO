c16ca95e84b9845183924163a95b4320c1e1c9e8
Sharlz <sharlyndee17@gmail.com>
2019-12-05 17:04:32 +0800

    initial

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className='container'>
-     <h1>NiHAO Mineral Resources International</h1>
+      <h1>NiHAO Mineral Resources International</h1>
+      <h2>hi</h2>
     </div>
   );
 }
